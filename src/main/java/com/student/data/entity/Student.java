4c8da04c5ed3348cdadfc7468cbaf625a10cf0ba
shikharkrdixit <sdixit362@gmail.com>
2022-10-18 02:03:48 +0530

    navbar added and lombok dependency added

--- src/main/java/com/student/data/entity/Student.java
+++ src/main/java/com/student/data/entity/Student.java
@@ -1,62 +1,28 @@
 package com.student.data.entity;
 
+import lombok.*;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "studentDetails")
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
 public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @NonNull private Long id;
 
     @Column(name = "first_name", nullable = false)
-    private String firstName;
+    @NonNull private String firstName;
 
     @Column(name = "last_name", nullable = false)
-    private String lastName;
+    @NonNull private String lastName;
 
     @Column(name = "email", nullable = false)
-    private String emailId;
+    @NonNull private String emailId;
 
-    public Student() {
-    }
-
-    public Student(String firstName, String lastName, String emailId) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.emailId = emailId;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmailId() {
-        return emailId;
-    }
-
-    public void setEmailId(String emailId) {
-        this.emailId = emailId;
-    }
 }
